c6a7adfc52a71889ba939ceb4bd26e88a66c9d8c
Daniel Kuznetsov <afc2008@yandex.ru>
2018-04-30 16:01:07 +0300

    Initial commit

---
+++ frontend/src/components/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="app">
+        Weather App Boilerplate
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
---
+++ frontend/src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+
+import App from '@/components/App.vue'
+
+import '@/global-styles/global.styl'
+
+new Vue({
+    el: '#app-root',
+    render: h => h(App)
+})
